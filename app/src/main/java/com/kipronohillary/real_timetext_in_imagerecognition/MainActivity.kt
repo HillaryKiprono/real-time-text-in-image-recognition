6e4b03481fed81a09c8b8e44eb1dc74bd2918987
Hillary Kiprono <kipronohillary240@gmail.com>
2022-10-14 11:29:40 +0300

    executor

--- app/src/main/java/com/kipronohillary/real_timetext_in_imagerecognition/MainActivity.kt
+++ app/src/main/java/com/kipronohillary/real_timetext_in_imagerecognition/MainActivity.kt
@@ -2,8 +2,11 @@ package com.kipronohillary.real_timetext_in_imagerecognition
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+    private val cameraExecutor: ExecutorService by lazy { Executors.newSingleThreadExecutor() }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
